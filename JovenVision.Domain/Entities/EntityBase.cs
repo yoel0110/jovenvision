9e97a4f5b5abf7a4f9ee7c2860afdb8aa88cc245
Yoel 4Devs <Yoellebron35@gmail.com>
2026-04-12 15:54:17 -0400

    feat: Add entities to Domain layer
    Entities:
    - Attendance
    - EntityBase
    - Event
    - Group
    - Member
    - Role
    -  Tracking
    - User

---
+++ JovenVision.Domain/Entities/EntityBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JovenVision.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ JovenVision.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JovenVision.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public bool Active { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public int? MemberId { get; set; }
+        public Member Member { get; set; }
+    }
+}
