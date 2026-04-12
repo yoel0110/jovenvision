fe0365a3f48c8a8d517aa890b054f31056e9cdf3
Yoel 4Devs <Yoellebron35@gmail.com>
2026-04-12 12:00:12 -0400

    feat: Add api with swagger

---
+++ JovenVision.Api/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.s
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
